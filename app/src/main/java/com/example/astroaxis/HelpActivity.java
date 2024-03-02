8a274a337dce9c4750e6c3242cd98132437a13f2
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-03-02 11:57:46 +0700

    Update HelpActivity

--- app/src/main/java/com/example/astroaxis/HelpActivity.java
+++ app/src/main/java/com/example/astroaxis/HelpActivity.java
@@ -1,17 +1,44 @@
 package com.example.astroaxis;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HelpActivity extends AppCompatActivity {
     private static final String TAG = "APP:HelpActivity";
 
+    private Button exit;
+    private ImageView openGitHubButton;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
         Log.d(TAG, "Start HelpActivity");
+
+        openGitHubButton = findViewById(R.id.openGithubButton);
+        exit = findViewById(R.id.exit);
+
+        openGitHubButton.setOnClickListener(view -> {
+            Log.d(TAG, "~~~ Pressing the button 'open_github' ~~~");
+
+            startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/LebedevSergeyVach/AstroAxis")));
+        });
+
+        exit.setOnClickListener(view -> {
+            Log.d(TAG, "~~~ Pressing the button 'exit' ~~~");
+
+            Intent intent = new Intent(HelpActivity.this, MainMenuActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        });
     }
 }
