3a2d48293ac3860ecae37d13c5f549674aba5328
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-04-03 14:13:38 +0700

    Updating 3D models

--- core/src/astro/axis/planet/libgdx/SaturnModel.java
+++ core/src/astro/axis/planet/libgdx/SaturnModel.java
@@ -1,67 +1,72 @@
 package astro.axis.planet.libgdx;
 
-
-import com.badlogic.gdx.ApplicationAdapter;
-
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.g3d.Environment;
-import com.badlogic.gdx.graphics.g3d.Model;
-import com.badlogic.gdx.graphics.g3d.ModelBatch;
-import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.JsonReader;
-import com.badlogic.gdx.utils.UBJsonReader;
+
 
 public class SaturnModel extends ApplicationAdapter {
-
     private PerspectiveCamera camera;
     private ModelBatch modelBatch;
-    private ModelInstance modelInstance;
+    private ModelInstance earthInstance;
+    private Model earthModel;
     private Environment environment;
+    private CameraInputController cameraController;
 
     @Override
-    public void create() {
-        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.position.set(0f, 0f, 2f); // Позиция камеры
-        camera.lookAt(0,0,0); // Направление взгляда камеры
-        camera.near = 1f; // Ближняя граница отображения
-        camera.far = 300f; // Дальняя граница отображения
-        camera.update();
-
+    public void create () {
         modelBatch = new ModelBatch();
+
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(0f, 0f, 3.5f);
+        camera.lookAt(0,0,0);
+        camera.near = 0.1f;
+        camera.far = 300f;
+
+        // Загрузка текстуры
+        Texture texture = new Texture(Gdx.files.internal("Saturn.jpg"));
+        texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        // Создание материала с текстурой
+        Material material = new Material(TextureAttribute.createDiffuse(texture));
+
+        // Создание 3D модели с применением текстуры
+        ModelBuilder modelBuilder = new ModelBuilder();
+        earthModel = modelBuilder.createSphere(2f, 2f, 2f, 20, 20, material,
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates);
+
+        earthInstance = new ModelInstance(earthModel);
+
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(1f, 1f, 1f, -1f, -1f, -1f));
 
-        // Загрузка модели из файла .fbx
-        G3dModelLoader modelLoader = new G3dModelLoader(new JsonReader());
-        Model model = modelLoader.loadModel(Gdx.files.internal("assets/Saturn.fbx"));
-
-        // Создание экземпляра модели и установка ее позиции в центр экрана
-        modelInstance = new ModelInstance(model);
-        modelInstance.transform.setToTranslation(0, 0, 0);
-
-
+        cameraController = new CameraInputController(camera);
+        Gdx.input.setInputProcessor(cameraController);
     }
 
     @Override
-    public void render() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+    public void render () {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+        cameraController.update();
+        camera.update();
+
         modelBatch.begin(camera);
-        modelBatch.render(modelInstance, environment);
+        modelBatch.render(earthInstance, environment);
         modelBatch.end();
     }
 
     @Override
-    public void dispose() {
+    public void dispose () {
         modelBatch.dispose();
+        earthModel.dispose();
     }
 }
