836a478b33ba7a9c00d192e8d8f9e6ba8378f22d
Sergey Lebedev <sergejlebedev31838@gmail.com>
2024-05-01 00:37:56 +0700

    Add NotificationHelper.java

---
+++ android/src/astro/axis/planet/libgdx/NotificationHelper.java
@@ -0,0 +1,39 @@
+package astro.axis.planet.libgdx;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import androidx.core.app.NotificationCompat;
+import android.app.NotificationChannel;
+import android.os.Build;
+import android.app.PendingIntent;
+import android.content.Intent;
+
+
+public class NotificationHelper {
+
+    private static final String CHANNEL_ID = "AstroAxisNotification";
+    private static final String CHANNEL_NAME = "AstroAxisNotification";
+    private static final String CHANNEL_DESCRIPTION = "This is my channel for notifications";
+
+    public static void showNotification(Context context, String title, String message, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Канал уведомлений для Android 8.0 и выше
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESCRIPTION);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        notificationManager.notify(0, builder.build());
+    }
+}
